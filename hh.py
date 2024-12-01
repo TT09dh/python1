ff6e853b4a27851b668a151fc97509af69e283fb
TT09dh <t9112048@gmail.com>
2024-12-01 19:53:33 +0300

    Add files via upload

---
+++ hh.py
@@ -0,0 +1,19 @@
+from random import randint
+random_min = 0
+random_max = 100
+truy = 10
+rd = randint(random_min , random_max)
+
+for i in range(truy):
+    try:
+        a = int(input('введите число от 1 до 100'))
+ 
+        if a == rd:
+            print('Вы угадали',(i + 1),'попыток')
+            break
+
+    except:
+        print('ВВЕДИТЕ ЧИСЛО!')
+    print('У вас осталось', truy - (i + 1),'попыток')
+
+    
